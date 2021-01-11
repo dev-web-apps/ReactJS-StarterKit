fe6bec2f624da097e376bd57e89bbf0a135457e8
samadali1 <sammada322@gmail.com>
2021-01-11 16:53:21 +0500

    auth-service

--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,71 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/core/styles';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
-import Navigations from './config/routes/index'
-import * as JwtService from './services/auth-services/jwtServices'
-import * as UserActions from './config/store/Actions/user.actions'
-import { useDispatch } from 'react-redux'
-import SplashScreen from '../src/screens/splash/index'
-
-
-function WithTheme(props) {
-    const dispatch = useDispatch();
-    const [isLoaded, setIsLoaded] = useState(false)
-    const theme = createMuiTheme({
-        palette: {
-            primary: {
-                main: "#000",
-            },
-            secondary: {
-                main: "#fff",
-            },
-            default: {
-                marin: "#f1f3f4"
-            },
-            tertiary: {
-                main: "#f0f0f0",
-                placeholder: "#3f3f3f",
-                text: "#434343",
-                heading: "#040404"
-            },
-            text: {
-                primary: "#191919",
-                secondary: "#969696"
-            }
-        },
-        typography: {
-            fontFamily: [
-                'PoppinsRegular',
-                'PoppinsMedium',
-            ].join(','),
-        },
-
-    });
-
-    useEffect(() => {
-        const token = JwtService.getAccessToken()
-        if (token!==null) {
-            JwtService.loginWithToken(token).then((res) => {
-                setIsLoaded(true)
-                console.log(res)
-                dispatch(UserActions.add_user(res))
-            }).catch((err) => {
-                JwtService.logout()
-            })
+const useStyles = makeStyles((theme) => {
+    return {
+        container: {
+            width: "100%",
+            height: "100%",
+            position:"fixed",
+            backgroundColor: theme.palette.primary.main,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
         }
-        else{
-            setIsLoaded(true)
+    }
+});
 
-        }
-    }, [])
-
+const SplashScreen = () => {
+    const classes = useStyles();
     return (
-        <ThemeProvider theme={theme}>
-            {isLoaded ? <Navigations /> :
-                <SplashScreen />}
-        </ThemeProvider>
-    );
+        <div className={classes.container}>
+            <CircularProgress color="secondary" />
+        </div>
+    )
 }
 
-export default WithTheme;
+export default SplashScreen
--- src/withThemeAndRoutes.js
+++ src/withThemeAndRoutes.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
 
 import Navigations from './config/routes/index'
+import * as JwtService from './services/auth-services/jwtServices'
+import * as UserActions from './config/store/Actions/user.actions'
+import { useDispatch } from 'react-redux'
+import SplashScreen from '../src/screens/splash/index'
+
 
 function WithTheme(props) {
-
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false)
     const theme = createMuiTheme({
         palette: {
             primary: {
@@ -14,13 +20,50 @@ function WithTheme(props) {
             secondary: {
                 main: "#fff",
             },
+            default: {
+                marin: "#f1f3f4"
+            },
+            tertiary: {
+                main: "#f0f0f0",
+                placeholder: "#3f3f3f",
+                text: "#434343",
+                heading: "#040404"
+            },
+            text: {
+                primary: "#191919",
+                secondary: "#969696"
+            }
+        },
+        typography: {
+            fontFamily: [
+                'PoppinsRegular',
+                'PoppinsMedium',
+            ].join(','),
         },
 
     });
 
+    useEffect(() => {
+        const token = JwtService.getAccessToken()
+        if (token!==null) {
+            JwtService.loginWithToken(token).then((res) => {
+                setIsLoaded(true)
+                console.log(res)
+                dispatch(UserActions.add_user(res))
+            }).catch((err) => {
+                JwtService.logout()
+            })
+        }
+        else{
+            setIsLoaded(true)
+
+        }
+    }, [])
+
     return (
         <ThemeProvider theme={theme}>
-            <Navigations />
+            {isLoaded ? <Navigations /> :
+                <SplashScreen />}
         </ThemeProvider>
     );
 }
